9923d8cea24def3fbad693a08064e9366de4e5ea
Mohammedalqaydi <113480778+Mohammedalqaydi@users.noreply.github.com>
2024-10-31 15:16:18 +0300

    add

--- bastion-runner-setup.sh
+++ bastion-runner-setup.sh
@@ -8,7 +8,6 @@ mkdir actions-runner; cd actions-runner
 curl -o actions-runner-linux-x64-2.320.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.320.0/actions-runner-linux-x64-2.320.0.tar.gz
 echo "93ac1b7ce743ee85b5d386f5c1787385ef07b3d7c728ff66ce0d3813d5f46900  actions-runner-linux-x64-2.320.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.320.0.tar.gz
-RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/Mohammedalqaydi/capstone-project-MohammedMustafa --token A3BZISTPAJYXJJ4ACY5Q4SLHELNYO
-#### the token is changed every refresh in browser
+RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/Mohammedalqaydi/capstone-project-MohammedMustafa --token A3BZISXTSBSC34TQQS5KTQLHENVP2
 
 RUNNER_ALLOW_RUNASROOT=1 ./run.sh 
